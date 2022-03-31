2c8407a82d93d1e7230e02c87ab1182994721266
oliverherlitschek <oliverherlitschek@gmail.com>
2022-03-31 13:15:57 +0200

    inc logo update

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -24,6 +24,17 @@
   pauseOnHover: false
 });
 
+  $('.ticker').slick({
+  speed: 5000,
+  autoplay: true,
+  autoplaySpeed: 0,
+  cssEase: 'linear',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  arrows: false
+});
+
 window.onload = function() {
     var months = ['januar', 'februar', 'marts', 'april', 'maj', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'december'];;
     var date = new Date();
